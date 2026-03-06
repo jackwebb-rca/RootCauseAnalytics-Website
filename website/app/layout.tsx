da225d0bf787ad029f7b4d825360c833210f2f5a
jackwebb-rca <jack.webb@rootcauseanalytics.com.au>
2026-03-06 18:33:12 +1100

    Add files via upload

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -71,6 +72,19 @@ export default function RootLayout({
         >
           Skip to main content
         </a>
+        {/* Google Analytics GA4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZMCESC0KQG"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZMCESC0KQG');
+          `}
+        </Script>
         {children}
       </body>
     </html>
